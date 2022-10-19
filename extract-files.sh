580512a662cbcd72ff8af5ed021ac26d68e86147
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-20 02:44:34 +0530

    sm6250-common: Bring back dolby secure decoders and fix crash
    Seems like new changes to c2 libs in android 12,
    broke these decoders. Suspicious changes:
    https://github.com/AOSPA/android_frameworks_av/commit/34821f568bc39602752ce056e7c4df57d12f6459
    https://github.com/AOSPA/android_frameworks_av/commit/34821f568bc39602752ce056e7c4df57d12f6459
    https://github.com/AOSPA/android_frameworks_av/commit/1d5b3f9539ccfd0b77a07a446cb0595c221497ce
    
    Copy the old libs and patch dolby binary to load
    it, in order to fix/workaround this problem.
    
    While we're at it, drop separate c2 manifest and
    add the dolby instance in the main manifest itself.
    
    This reverts commits b6bf2345d3b687701ad626d34f58cf1ea4c9f966 and b9848af2ca6a87fbce95cee101d193e453e21923.
    
    Test: Dolby Vision playback in Netflix.
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -58,12 +58,16 @@ function blob_fixup() {
     vendor/etc/seccomp_policy/vendor.qti.hardware.dsp.policy)
     echo 'madvise: 1' >> ${2}
     ;;
-    odm/etc/vintf/manifest/c2_manifest_xiaomi.xml)
-    sed -i 's|<hal format="hidl">|<hal format="hidl" override="true">|g' "${2}"
-    sed -i "/ozoaudio/d" "${2}"
-    ;;
     vendor/bin/hw/dolbycodec2)
     patchelf --replace-needed libavservices_minijail_vendor.so libavservices_minijail.so "${2}"
+    patchelf --replace-needed libcodec2_hidl@1.0.so libcodec2_hidl@1.0.stock.so "${2}"
+    ;;
+    vendor/lib/libcodec2_hidl@1.0.stock.so)
+    patchelf --set-soname libcodec2_hidl@1.0.stock.so "${2}"
+    patchelf --replace-needed libcodec2_vndk.so libcodec2_vndk.stock.so "${2}"
+    ;;
+    vendor/lib/libcodec2_vndk.stock.so)
+    patchelf --set-soname libcodec2_vndk.stock.so "${2}"
     ;;
     esac
 }
