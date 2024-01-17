0731149ca7a4c26a2dc737c2ab1817a412f8ab1a
userariii <abhattacharjee717@gmail.com>
2024-01-17 22:31:00 +0530

    sm6250-common: Copy AOSP bluetooth permissions
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- common.mk
+++ common.mk
@@ -104,6 +104,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Bluetooth - permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
